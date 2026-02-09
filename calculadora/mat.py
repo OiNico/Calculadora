d3ceef6fd4a36fa31fa186eabcb37891564c98b6
Nicolas Gomes <soneca144p@gmail.com>
2026-02-09 19:34:25 -0300

    Calculadora em Python

---
+++ calculadora/mat.py
@@ -0,0 +1,19 @@
+
+class Mat:
+    
+    #soma
+    def Soma(primeiroValor, segundoValor):
+        return primeiroValor + segundoValor
+    
+    #subtração
+    def Subtracao(primeiroValor, segundoValor):
+        return primeiroValor - segundoValor
+    
+    #multiplicação
+    def Multiplicacao(primeiroValor, segundoValor):
+        return primeiroValor * segundoValor
+    
+    #divisão
+    def divisao(primeiroValor, segundoValor):
+        return primeiroValor / segundoValor
+    
